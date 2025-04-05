a1132c6f69a2b7070005fba95488a8c90b4f8169
DreuxX <dreuxx@DreuxXs-MacBook-Pro.local>
2025-04-05 13:32:40 -0400

    Versión inicial de Grammar Checker Extension ML

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    popup: './popup.js',
+    background: './background.js',
+    content: './content.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
